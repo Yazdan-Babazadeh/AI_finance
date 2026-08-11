38a37cc60b319566ca07cc5638cfdf2c1e1a24c1
Yazdan Babazadeh <yazdan_babazadeh@outlook.com>
2026-08-11 07:48:54 -0400

    Completed the backtesting on optimized portfolio, calculated sharpe ratio and performance and compared to the previous portfolio construction

--- main.py
+++ main.py
@@ -219,59 +219,96 @@ backtest["CumulativeReturn"] = (1+backtest["LongShortReturn"]).cumprod()
 #plt.plot(backtest.index,backtest["CumulativeReturn"])
 #plt.show()
 
-#sharp_ratio = (backtest["LongShortReturn"].mean()*np.sqrt(12.6))/backtest["LongShortReturn"].std()
+sharp_ratio = (backtest["LongShortReturn"].mean()*np.sqrt(12.6))/backtest["LongShortReturn"].std()
+
+print("sharpe ratio for 20-80 portfolio equals:",sharp_ratio)
 
 
-#### Construction of portfolio optimizer
+#### Construction of portfolio optimization
 
 
 lookback = 60
 
-current_date = rebalance_dates.iloc[25]
-
-#today is the current_date, what portfolio should i hold for next 20 days
-
-current_portfolio = portfolio_data[portfolio_data["Date"]==current_date].copy()
-
-current_tickers = current_portfolio["Ticker"].tolist()
-
-Mu = current_portfolio["PredictedReturn"].values
-
-returns_wide = data.pivot(index= "Date", columns = "Ticker", values = "DailyReturn")
-
-returns_wide = returns_wide.dropna()
-
-past_returns = returns_wide.loc[returns_wide.index < current_date, current_tickers]
-
-past_returns = past_returns.tail(lookback)
-                       
-
-Sigma = past_returns.cov().values
-
-import cvxpy as cp
-
-n = len(Mu)
-
-w = cp.Variable(n)
-
-expected_portfolio_return = Mu @ w
-
-portfolio_variance = cp.quad_form(w,Sigma)
-
-risk_aversion = 5
-
-objective = cp.Maximize(expected_portfolio_return - risk_aversion*portfolio_variance)
-
-constraints = [cp.sum(w) ==0, cp.norm1(w) <=2, w<= 0.05, w>=-0.05]
-
-problem = cp.Problem(objective, constraints)
-
-problem.solve()
-
-optimal_weights = w.value
-
-current_portfolio["Weights"] = optimal_weights
 
 
-print( current_portfolio[["Ticker","PredictedReturn","Weights"]].sort_values("Weights",ascending=False))
+portfolio_results = []
 
+
+for current_date in rebalance_dates:
+
+
+    #today is the current_date, what portfolio should i hold for next 20 days
+
+    current_portfolio = portfolio_data[portfolio_data["Date"]==current_date].copy()
+
+    current_tickers = current_portfolio["Ticker"].tolist()
+
+    Mu = current_portfolio["PredictedReturn"].values
+
+    returns_wide = data.pivot(index= "Date", columns = "Ticker", values = "DailyReturn")
+
+    returns_wide = returns_wide.dropna()
+
+    past_returns = returns_wide.loc[returns_wide.index < current_date, current_tickers]
+
+    past_returns = past_returns.tail(lookback)
+                           
+
+    Sigma_daily = past_returns.cov().values
+
+    Sigma = 20*Sigma_daily
+
+    import cvxpy as cp
+
+    n = len(Mu)
+
+    w = cp.Variable(n)
+
+    expected_portfolio_return = Mu @ w
+
+    portfolio_variance = cp.quad_form(w,Sigma)
+
+    risk_aversion = 5
+
+    objective = cp.Maximize(expected_portfolio_return - risk_aversion*portfolio_variance)
+
+    constraints = [cp.sum(w) ==0, cp.norm1(w) <=2, w<= 0.05, w>=-0.05]
+
+    problem = cp.Problem(objective, constraints)
+
+    problem.solve()
+
+    optimal_weights = w.value
+
+    current_portfolio["Weights"] = optimal_weights
+
+
+   
+
+    predicted_portfolio_return = (current_portfolio["Weights"]*current_portfolio["PredictedReturn"]).sum()
+
+   
+
+    realized_portfolio_return = (current_portfolio["Weights"]*current_portfolio[target]).sum()
+
+    
+    portfolio_results.append({"Date": current_date, "PredictedReturn": predicted_portfolio_return,"RealizedReturn": realized_portfolio_return})
+
+
+portfolio_results = pd.DataFrame(portfolio_results)
+
+
+portfolio_results["CumulativeRealizedReturn"] = ( 1 + portfolio_results["RealizedReturn"]).cumprod()
+
+
+portfolio_results["CumulativePredictedReturn"] = ( 1 + portfolio_results["PredictedReturn"]).cumprod()
+
+
+####### Sharpe Ratio
+
+
+periods_per_year = 252/20
+
+optimized_sharpe = (portfolio_results["RealizedReturn"].mean()*np.sqrt(12.6))/portfolio_results["RealizedReturn"].std()
+
+print("Optimized Sharpe Ratio:",optimized_sharpe)
