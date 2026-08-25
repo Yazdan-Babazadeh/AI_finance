a424692fbf75eda4a5dab984710913c2c3e2d9e6
Yazdan Babazadeh <yazdan_babazadeh@outlook.com>
2026-08-25 08:21:22 -0400

    added Lasso and also a library of factors, needs to fine tune alpha for each future horizon

--- learning_engine.py
+++ learning_engine.py
@@ -1,8 +1,9 @@
 import statsmodels.api as sm
+from sklearn.linear_model import Lasso
 
 class LearningEngine:
 
-    def __init__(self, model_type, train, validation, test, standardized_features,factor_engine,jump):
+    def __init__(self, model_type, train, validation, test, standardized_features,factor_engine,jump,alpha):
 
         self.model_type = model_type
         self.train_data = train
@@ -12,24 +13,37 @@ class LearningEngine:
         self.model = None
         self.factor_engine = factor_engine
         self.jump = jump
+        self.alpha = alpha
     def train(self):
-
+ 
+        x_train = self.train_data[self.standardized_features]
+        y_train = self.train_data[self.factor_engine.target]
+        
         if self.model_type == "LR":
 
-            x_train = self.train_data[self.standardized_features]
-            y_train = self.train_data[self.factor_engine.target]
 
             x_train = sm.add_constant(x_train)
-
-
-
+            
             self.model = sm.OLS(y_train,x_train).fit(cov_type="cluster",cov_kwds={"groups": self.train_data["Date"]})
            # print(self.model.summary())
+
+        elif self.model_type == "LASSO":
+            self.model = Lasso(
+                    alpha = self.alpha,
+                    fit_intercept = True,
+                    max_iter = 10000
+                    )
+            self.model.fit(
+                    x_train,
+                    y_train
+                    )
         return self.model
 
+
     def validate(self):
+        x_validation = self.validation_data[self.standardized_features]
         if self.model_type == "LR":
-            x_validation = self.validation_data[self.standardized_features]
+
             x_validation = sm.add_constant(
                 x_validation,
                 has_constant="add"
@@ -37,23 +51,28 @@ class LearningEngine:
             self.validation_data["PredictedReturn"] = self.model.predict(
                 x_validation
             )
+
+        elif self.model_type == "LASSO":
+            
+            self.validation_data["PredictedReturn"] = self.model.predict(x_validation)
+
             # Take every jump-th unique date
-            validation_dates = (
-                self.validation_data["Date"]
-                .drop_duplicates()
-                .sort_values()
-            )
-            sampled_dates = validation_dates.iloc[
-                ::self.jump
-            ]
+        validation_dates = (
+            self.validation_data["Date"]
+            .drop_duplicates()
+            .sort_values()
+        )
+        sampled_dates = validation_dates.iloc[
+            ::self.jump
+        ]
             # Keep all stocks for those dates
-            independent_validation = self.validation_data[
-                self.validation_data["Date"].isin(sampled_dates)
-            ]
-            actual = independent_validation[self.factor_engine.target]
-            predicted = independent_validation["PredictedReturn"]
-            oos_r2 = 1 - (
-                ((actual - predicted) ** 2).sum()
-                / (actual ** 2).sum()
-            )
-            return independent_validation, oos_r2
+        independent_validation = self.validation_data[
+            self.validation_data["Date"].isin(sampled_dates)
+        ]
+        actual = independent_validation[self.factor_engine.target]
+        predicted = independent_validation["PredictedReturn"]
+        oos_r2 = 1 - (
+            ((actual - predicted) ** 2).sum()
+            / (actual ** 2).sum()
+        )
+        return independent_validation, oos_r2
--- main.py
+++ main.py
@@ -14,10 +14,11 @@ from preprocess_engine import PreprocessEngine
 from learning_engine import LearningEngine
 from backtest_engine import BacktestEngine
 
+
+##### HYPER PARAMETERS ######
+
 benchmark = "SPY"
-
-
-# Gathering Data
+learning_model = "LASSO"
 
 start_date = "2010-01-01"
 end_date = "2026-01-01"
@@ -25,6 +26,12 @@ validation_date = "2020-01-01"
 test_date = "2026-01-01"
 
 forecast_horizon_values = [5,10,20,30,40,50,60,70,80,90,100,110,120,130,140,150]
+
+
+
+alpha = 0.005
+
+############################
 
 max_horizon = max(forecast_horizon_values)
 full_results = []
@@ -41,19 +48,29 @@ for forecast_horizon in forecast_horizon_values:
     
     #Focusing on 4 factors, Momentum, Volatility, Volume Ratio, Short term Return
     
-    ### Momentum
     
     factor_engine = FactorEngine(data_engine)
     
+    factor_engine.add_momentum(100,5)
     factor_engine.add_momentum(60,5)
+    factor_engine.add_momentum(10,5)
+    factor_engine.add_momentum(20,5)
+    
+    factor_engine.add_volatility(100)
     factor_engine.add_volatility(20)
+    factor_engine.add_volatility(10)
+    factor_engine.add_volatility(60)
+
+    factor_engine.add_volume(100)
     factor_engine.add_volume(20)
+    factor_engine.add_volume(10)
+    factor_engine.add_volume(60)
+
+    factor_engine.add_shortterm_return(20)
+    factor_engine.add_shortterm_return(10)
     factor_engine.add_shortterm_return(5)
     
     model_data = factor_engine.build_model_data()
-    
-    
-    
     
     
     preprocess_engine = PreprocessEngine(model_data,factor_engine)
@@ -67,42 +84,72 @@ for forecast_horizon in forecast_horizon_values:
     results = []
     features = factor_engine.features
     
-    for n in range(1,len(features)+1):
-    
-        for subset in combinations(features,n):
-    
-    
-            standardized_subset = [feature +"_z" for feature in subset]
-    
-    
-            learning_engine = LearningEngine("LR",train_data,validation_data.copy(),test_data,standardized_subset,factor_engine,jump)
-            model = learning_engine.train()
-    
-            _, oos_r2 = learning_engine.validate()
-    
-            results.append({
-                "Factors": subset,
-                "N_Factors": len(subset),
-                "Validation_R2": oos_r2
-                })
-    
-    results = pd.DataFrame(results)
-    results = results.sort_values("Validation_R2",ascending=False).reset_index(drop=True)
-    best = results.loc[0]
 
-    full_results.append({
-        "Forecast_Horizon": forecast_horizon,
-        "Factors": best["Factors"],
-        "Validation_R2": best["Validation_R2"],
-        "Train_Rows": len(train_data),
-        "Validation_Rows": len(validation_data),
-        "Validation_Dates": validation_data["Date"].nunique()
-    })
+    if learning_model == "LASSO":
 
+        learning_engine = LearningEngine(learning_model,train_data,validation_data.copy(),test_data,standardized_features,factor_engine,jump,alpha)
+        model = learning_engine.train()
+
+        independent_validation,oos_r2 = learning_engine.validate()
+
+        selected_factors = []
+
+        for feature,coefficient in zip(features,model.coef_):
+            if coefficient !=0:
+
+                selected_factors.append((feature,coefficient))
+
+        full_results.append({
+
+            "Forecast_Horizon": forecast_horizon,
+            "Alpha": alpha,
+            "Selected_Factors": selected_factors,
+            "N Selected": len(selected_factors),
+            "Validation_R2" : oos_r2,
+            "Train_Rows": len(train_data),
+            "Validation Rows": len(independent_validation),
+            "Validation Dates": (independent_validation["Date"].nunique())
+            })
+
+
+
+    elif learning_model == "LR":
+        
+        for n in range(1,len(features)+1):
+        
+            for subset in combinations(features,n):
+        
+        
+                standardized_subset = [feature +"_z" for feature in subset]
+        
+        
+                learning_engine = LearningEngine(learning_model,train_data,validation_data.copy(),test_data,standardized_subset,factor_engine,jump,alpha)
+                model = learning_engine.train()
+        
+                _, oos_r2 = learning_engine.validate()
+        
+                results.append({
+                    "Factors": subset,
+                    "N_Factors": len(subset),
+                    "Validation_R2": oos_r2
+                    })
+        
+        results = pd.DataFrame(results)
+        results = results.sort_values("Validation_R2",ascending=False).reset_index(drop=True)
+        best = results.loc[0]
+    
+        full_results.append({
+            "Forecast_Horizon": forecast_horizon,
+            "Factors": best["Factors"],
+            "Validation_R2": best["Validation_R2"],
+            "Train_Rows": len(train_data),
+            "Validation_Rows": len(validation_data),
+            "Validation_Dates": validation_data["Date"].nunique()
+        })
 full_results = pd.DataFrame(full_results)
-full_results.to_csv(f"results_jump_{jump}.csv",index=False)
+full_results.to_csv(f"results_jump_{jump}_{learning_model}.csv",index=False)
 print(full_results)
-
+    
 plt.scatter(full_results["Forecast_Horizon"],full_results["Validation_R2"])
 plt.show()
     ########## Backtesting:
